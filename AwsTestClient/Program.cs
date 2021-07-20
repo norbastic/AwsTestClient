b89fc34d4e6f8c114f78aed8228016a8f2b53f18
Norbert Farago <nfarago@chemaxon.com>
2021-07-20 15:10:52 +0200

    Initial commit

---
+++ AwsTestClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Amazon.RDS;
+
+namespace AwsTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
